be193a866dafe3f42cc56f40975bfde661ff9659
Sebastian <mejiasebastian333@gmail.com>
2026-03-02 08:22:27 -0500

    tabla y archvivos config y coplementos de eso archivos subidos

---
+++ src/app.js
@@ -0,0 +1,9 @@
+import express from "express";
+
+export const app = express();
+
+app.use(express.json())
+
+app.post('/api/simulacro/PRUEBA DB AM', (req, res) =>{
+    
+})
---
+++ src/server.js
@@ -0,0 +1,22 @@
+import { app } from "./app.js";
+import { env } from "../.env";
+import { connectMongoDB } from "./config/mongoDB.js";
+import { createTables } from "./config/postgres.js";
+
+
+//copiar y pegar 
+try {
+    console.log('Conecting to MongoDB...');
+    await connectMongoDB();
+    console.log('Conecting to PostgresSQL...');
+    await createTables()
+    app.listen(env.port, () => {
+        console.log(`server running on port ${env.port}`);
+        console.log(`Endpoint disponibles:`);
+
+    })
+
+} catch (error) {
+    console.log(error);
+    process.exit(1)
+}
